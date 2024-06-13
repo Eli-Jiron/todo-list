aa88cbdda2cd12027c6ebfc1767e70419c1ad34b
Eli-Jiron <elianguevara28@gmail.com>
2024-06-12 21:43:58 -0600

    Funcion: renderizar tareas

---
+++ src/js/render.js
@@ -0,0 +1,46 @@
+let taskList = document.getElementById("taskList");
+
+export const renderTasks = (task) => {
+  //Etiquetas//
+  let borderTask = document.createElement("div");
+  let divContent = document.createElement("div");
+  let divTask = document.createElement("div");
+  let divBtn = document.createElement("div");
+  let checkbox = document.createElement("input");
+  let btnDelete = document.createElement("button");
+  let btnEdit = document.createElement("button");
+  let btnDeleteImg = document.createElement("img");
+  let btnEditImg = document.createElement("img");
+
+  //Contenido//
+  btnDeleteImg.src = "https://autumn.revolt.chat/attachments/wFIHXe0d91Rb_yLIbiSq9ZK9NyB-QpKEuTHIC1RxLB";
+  btnEditImg.src = "https://autumn.revolt.chat/attachments/PsiPkuNbVmVeeaSwfPqXvap1luWr87prb5XP3GaYIb";
+  checkbox.type = "checkbox";
+
+  //Clases y Ids//
+  if (task.status) {
+    checkbox.className = "unchecked";
+  } else {
+    checkbox.className = "checked";
+  }
+  borderTask.className = "borderTask";
+  divContent.className = "divContent";
+  divContent.id = task.id;
+  btnDelete.className = "btnDelete";
+  btnEdit.className = "btnEdit";
+  btnDeleteImg.className = "btnDeleteImg";
+  btnEditImg.className = "btnEditImg";
+  divBtn.className = "divBtn";
+
+  //Armado//
+  btnDelete.appendChild(btnDeleteImg);
+  btnEdit.appendChild(btnEditImg);
+  divBtn.appendChild(btnEdit);
+  divBtn.appendChild(btnDelete);
+  divTask.textContent = task.task;
+  divContent.appendChild(checkbox);
+  divContent.appendChild(divTask);
+  divContent.appendChild(divBtn);
+  borderTask.appendChild(divContent);
+  taskList.appendChild(borderTask);
+};
